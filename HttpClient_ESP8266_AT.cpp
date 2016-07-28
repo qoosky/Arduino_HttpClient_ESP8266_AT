3cd06a5bc2d5a783cca7245c94f7fd9a8074fd1b
qoosky <qoosky@users.noreply.github.com>
2016-07-29 00:42:00 +0900

    Add ESP8266's Health check functions

--- HttpClient_ESP8266_AT.cpp
+++ HttpClient_ESP8266_AT.cpp
@@ -9,3 +9,28 @@ HttpClient_ESP8266_AT::HttpClient_ESP8266_AT(uint32_t rxPin, uint32_t txPin, uin
 HttpClient_ESP8266_AT::~HttpClient_ESP8266_AT() {
     delete m_serial;
 }
+
+void HttpClient_ESP8266_AT::rxClear() {
+    while(m_serial->available() > 0) m_serial->read();
+}
+
+bool HttpClient_ESP8266_AT::statusAT() {
+    rxClear();
+    m_serial->println("AT");
+
+    // check "OK" is received
+    String str;
+    char c;
+    unsigned long start = millis();
+    const uint32_t timeout = 1000;
+    const String target = "OK";
+    while (millis() - start < timeout) {
+        while(m_serial->available() > 0) {
+            c = m_serial->read(); // read 1 byte
+            if(c == '\0') continue;
+            str += c;
+        }
+        if (str.indexOf(target) != -1) return true;
+    }
+    return false;
+}
--- HttpClient_ESP8266_AT.h
+++ HttpClient_ESP8266_AT.h
@@ -7,10 +7,22 @@
 class HttpClient_ESP8266_AT
 {
  public:
+    // rxPin: Wire this to Tx Pin of ESP8266
+    // txPin: Wire this to Rx Pin of ESP8266
     HttpClient_ESP8266_AT(uint32_t rxPin, uint32_t txPin, uint32_t baud = 115200);
+
+    // Destructor
     ~HttpClient_ESP8266_AT();
 
+ public:
+    // Health check of m_serial
+    bool statusAT();
+
  private:
+    // Clear rx buffer
+    void rxClear();
+
+    // AT command serial interface for ESP8266
     SoftwareSerial *m_serial;
 };
 
