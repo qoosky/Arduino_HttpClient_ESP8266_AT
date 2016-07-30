c7b4f88521a68f956d030414084e4e7631bd3d96
qoosky <qoosky@users.noreply.github.com>
2016-07-30 21:11:56 +0900

    Add Functions to connect to an AP

--- HttpClient_ESP8266_AT.cpp
+++ HttpClient_ESP8266_AT.cpp
@@ -19,6 +19,7 @@ HttpClient_ESP8266_AT::HttpClient_ESP8266_AT(HardwareSerial &serial) :
 }
 
 HttpClient_ESP8266_AT::~HttpClient_ESP8266_AT() {
+    disconnect();
     if(m_rxPin != 0 && m_txPin !=0) delete m_serial;
 }
 
@@ -26,28 +27,75 @@ void HttpClient_ESP8266_AT::rxClear() {
     while(m_serial->available() > 0) m_serial->read();
 }
 
-bool HttpClient_ESP8266_AT::statusAT() {
-    rxClear();
-    m_serial->println("AT");
-
-    // check "OK" is received
-    String str;
+bool HttpClient_ESP8266_AT::checkATResponse(String &buf, String target, uint32_t timeout) {
+    buf = "";
     char c;
     unsigned long start = millis();
-    const uint32_t timeout = 1000;
-    const String target = "OK";
     while (millis() - start < timeout) {
         while(m_serial->available() > 0) {
             c = m_serial->read(); // 1 byte
             if(c == '\0') continue;
-            str += c;
+            buf += c;
         }
-        if (str.indexOf(target) != -1) return true;
+        if (buf.indexOf(target) != -1) return true;
+    }
+    return false;
+}
+
+bool HttpClient_ESP8266_AT::checkATResponse(String target, uint32_t timeout) {
+    String buf;
+    return checkATResponse(buf, target, timeout);
+}
+
+bool HttpClient_ESP8266_AT::statusAT() {
+    rxClear();
+    m_serial->println("AT");
+    return checkATResponse();
+}
+
+bool HttpClient_ESP8266_AT::restart() {
+    rxClear();
+    m_serial->println("AT+RST");
+    if(!checkATResponse()) return false;
+    delay(2000);
+    unsigned long start = millis();
+    while(millis() - start < 3000) {
+        if(statusAT()) {
+            delay(1500);
+            return true;
+        }
+        delay(100);
     }
     return false;
 }
 
 bool HttpClient_ESP8266_AT::connect(String ssid, String password) {
     rxClear();
-    return true; // TODO
+    m_serial->println("AT+CWMODE_DEF=1"); // 1: station(client) mode, 2: softAP(server) mode, 3: 1&2
+    if(!(checkATResponse() && restart())) return false; // change "DEF"ault cwMode and restart
+
+    // Connect to an AP
+    rxClear();
+    m_serial->print("AT+CWJAP_DEF=\"");
+    m_serial->print(ssid);
+    m_serial->print("\",\"");
+    m_serial->print(password);
+    m_serial->println("\"");
+    return checkATResponse("OK", 10000);
+}
+
+bool HttpClient_ESP8266_AT::disconnect() {
+    rxClear();
+    m_serial->println("AT+CWQAP");
+    return checkATResponse();
+}
+
+bool HttpClient_ESP8266_AT::statusWiFi() {
+    String buf;
+    rxClear();
+    m_serial->println("AT+CIPSTATUS");
+    checkATResponse(buf, "OK", 10000);
+    uint32_t index = buf.indexOf(":");
+    uint8_t stat = buf.substring(index + 1, index + 2).toInt();
+    return (stat != 5);
 }
--- HttpClient_ESP8266_AT.h
+++ HttpClient_ESP8266_AT.h
@@ -12,7 +12,7 @@ class HttpClient_ESP8266_AT
     uint32_t m_txPin;
 
     // AT command serial interface for ESP8266
-    Stream *m_serial; // super class of SoftwareSerial and HardwareSerial
+    Stream *m_serial; // base class of SoftwareSerial and HardwareSerial
 
  public:
     // Three types of the constructors:
@@ -28,15 +28,26 @@ class HttpClient_ESP8266_AT
     ~HttpClient_ESP8266_AT();
 
  public:
-    // Health check of m_serial
+    // Health check of the serial interface
     bool statusAT();
 
-    // connect ESP8266 to WiFi network
+    // Health check of the WiFi connection
+    bool statusWiFi();
+
+    // Connect/Disconnect ESP8266 to/from WiFi network
     bool connect(String ssid, String password);
+    bool disconnect();
 
  private:
     // Clear rx buffer
     void rxClear();
+
+    // Restart (Reset) ESP8266
+    bool restart();
+
+    // Check the response for the last AT command is "OK"
+    bool checkATResponse(String target = "OK", uint32_t timeout = 1000);
+    bool checkATResponse(String &buf, String target = "OK", uint32_t timeout = 1000); // store the data into buffer
 };
 
 #endif // #ifndef HTTPCLIENT_ESP8266_AT_H_
